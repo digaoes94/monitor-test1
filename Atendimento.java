51a82949a490472d36b4e761d26cdf2e0df2e33c
Rodrigo Araujo Schenberg <rasch.dev@gmail.com>
2024-04-03 15:47:18 -0300

    Create Atendimento to manipulate Client tending

---
+++ Atendimento.java
@@ -0,0 +1,45 @@
+package controller;
+
+import entities.Cliente;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Atendimento {
+	private List<Cliente> filaAtendimento = new ArrayList<Cliente>();
+	private final int filaLimite = 500;
+	
+	//  constructor
+	public Atendimento() {
+		this.filaAtendimento = new ArrayList<Cliente>();
+	}
+	
+	//  get set fila
+	public List<Cliente> getFilaAtendimento() {
+		return filaAtendimento;
+	}
+	public void setFilaAtendimento(List<Cliente> filaAtendimento) {
+		this.filaAtendimento = filaAtendimento;
+	}
+
+	public String entrarNaFila(Cliente cliente) {
+		if (filaAtendimento.size() >= this.filaLimite) {
+			return "A fila está cheia.";
+		}
+		else {
+			filaAtendimento.add(cliente);
+			return "Cliente adicionado à lista.";
+		}
+	}
+	
+	public Cliente atender() {
+		
+		if (filaAtendimento.size() == 0) {
+			return null;
+		}
+		else {
+			Cliente aux = filaAtendimento.get(0);
+			filaAtendimento.remove(0);
+			return aux;
+		}
+	}
+}
